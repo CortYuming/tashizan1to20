0df94d3e1c156fc348f8e3a15b97ebf1c8373c4a
ITO Takashi <cortyuming@gmail.com>
2024-04-06 07:23:02 +0900

    First commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const problem = document.getElementById('problem');
+const answer = document.getElementById('answer');
+const totalCount = document.getElementById('totalCount')
+let count = 1
+
+function generateProblem() {
+    const num1 = Math.floor(Math.random() * 20) + 1;
+    const num2 = Math.floor(Math.random() * 20) + 1;
+
+    const circles1 = formatString('●'.repeat(num1));
+    const circles2 = formatString('●'.repeat(num2));
+
+    problem.textContent = `${circles1}\n + \n${circles2}`;
+    answer.textContent = `${num1} + ${num2} = ${num1 + num2}`;
+}
+
+function addPadding(str, num) {
+  return str.padEnd(num, "◯");
+}
+
+function formatString(str) {
+  let result = "";
+  let lines = [];
+
+  for (let i = 0; i < str.length; i += 5) {
+    lines.push(addPadding(str.slice(i, i + 5), 5));
+  }
+
+  result = lines.join("\n");
+  return result;
+}
+
+
+generateProblem();
+
+
+function showProblem() {
+  generateProblem();
+  answer.style.display = 'none';
+
+  count += 1
+  totalCount.innerHTML = count + " もんめ";;
+}
+
+function showAnswer() {
+  answer.style.display = 'block';
+}
