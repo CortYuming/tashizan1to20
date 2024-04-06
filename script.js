ccd30086a6cc0b46d3d49a7720ab4f4b49a2f926
ITO Takashi <cortyuming@gmail.com>
2024-04-07 08:05:21 +0900

    refactor: rename and indent, etc

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-const problem = document.getElementById('problem');
+const expression = document.getElementById('expression');
 const answer = document.getElementById('answer');
 const totalCount = document.getElementById('totalCount')
 let count = 1
 
 showTotalCount(count);
 
-function generateProblem() {
-    const num1 = Math.floor(Math.random() * 20) + 1;
-    const num2 = Math.floor(Math.random() * 20) + 1;
+function generateExpression() {
+  const num1 = Math.floor(Math.random() * 20) + 1;
+  const num2 = Math.floor(Math.random() * 20) + 1;
 
-    const circles1 = formatString('●'.repeat(num1));
-    const circles2 = formatString('●'.repeat(num2));
+  const circles1 = formatString('●'.repeat(num1));
+  const circles2 = formatString('●'.repeat(num2));
 
-    problem.textContent = `${circles1}\n + \n${circles2}`;
-    answer.textContent = `${num1} + ${num2} = ${num1 + num2}`;
+  expression.textContent = `${circles1}\n + \n${circles2}`;
+  answer.textContent = `${num1} + ${num2} = ${num1 + num2}`;
 }
 
 function addPadding(str, num) {
-  return str.padEnd(num, "◯");
+  return str.padEnd(num, '◯');
 }
 
 function formatString(str) {
-  let result = "";
+  let result = '';
   let lines = [];
 
   for (let i = 0; i < str.length; i += 5) {
     lines.push(addPadding(str.slice(i, i + 5), 5));
   }
 
-  result = lines.join("\n");
+  result = lines.join('\n');
   return result;
 }
 
 
-generateProblem();
+generateExpression();
 
 
-function showProblem() {
-  generateProblem();
+function showExpression() {
+  generateExpression();
   answer.style.display = 'none';
 
   count += 1
@@ -45,7 +45,7 @@ function showProblem() {
 }
 
 function showTotalCount(count) {
-  totalCount.innerHTML = count + " もんめ";;
+  totalCount.innerHTML = count + ' もんめ';;
 }
 
 function showAnswer() {
@@ -55,5 +55,5 @@ function showAnswer() {
 function resetCount() {
   count = 0;
   showTotalCount(count);
-  generateProblem();
+  generateExpression();
 }
